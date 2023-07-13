a9ad6671dd13c803d241627dbd171a5a8bb71472
Natalka333 <126109559+Natalka333@users.noreply.github.com>
2023-07-13 16:37:16 +0300

    add library

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+galleryEl.classList.add();
+function makeGalleryItemsMarkup(items) {
+  return items
+    .map(({ original, preview, description }) => {
+      return `
+		<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+  </li>`;
+    })
+    .join('');
+}
+galleryEl.innerHTML = makeGalleryItemsMarkup(galleryItems);
 
-console.log(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+// console.log(galleryItems);
